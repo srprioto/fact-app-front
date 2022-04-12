ee9f855e0f3ff1d47472483facf062a88fe17bb6
prioto <hailprioto@gmail.com>
2022-04-12 16:46:58 -0500

    correciones 8

--- src/components/transferencia/AlertaTransferencia.tsx
+++ src/components/transferencia/AlertaTransferencia.tsx
@@ -19,6 +19,7 @@ export const AlertaTransferencia = ({ idLocal, actualizarDatos }:alertaTransfere
 
     useEffect(() => {
         getTransaccionesLocal();
+        
     }, [])
     
     const getTransaccionesLocal = async () => { 
