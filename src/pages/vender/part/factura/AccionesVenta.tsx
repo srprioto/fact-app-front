ba5bb107f40bc1938c1d39fd0a9ef7abd37cefd9
prioto <hailprioto@gmail.com>
2022-06-18 16:45:52 -0500

    factura con validacion de datos4

--- src/pages/vender/part/factura/AccionesVenta.tsx
+++ src/pages/vender/part/factura/AccionesVenta.tsx
@@ -1,6 +1,8 @@
 import { BiArrowBack, BiCaretRight } from "react-icons/bi"
+import { useAuth } from "../../../../auth/useAuth";
 import { BtnOnOff2 } from "../../../../components/btns/BtnOnOff2";
 import { LoadSwitchBtn2 } from "../../../../components/btns/LoadSwitchBtn2"
+import { Roles } from "../../../../resources/dtos/RolesDto";
 
 interface rapidaVenta {
     loadVenta:boolean;
@@ -19,9 +21,18 @@ export const AccionesVenta = ({
 }:rapidaVenta) => {
 
     const verCaja:boolean = verificarVender();
+    const auth = useAuth();
+    
 
     return (
-        <div className="grid-3 gap acciones-venta">
+        <div className={
+            "grid-3 gap " + 
+            (
+                auth.rol === Roles.SALLER 
+                ? "acciones-venta-vendedor"
+                : "acciones-venta"
+            )
+        }>
             <div className="grid-6">
                 <button className="btn btn-primary" onClick={() => setShowWindow(1)}>
                     {/* <BiRightArrowAlt /> */}
