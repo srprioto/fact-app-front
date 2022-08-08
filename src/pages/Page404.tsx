9ec0b55b3d9031ecc829b08e1ef991f27046069b
prioto <hailprioto@gmail.com>
2022-08-07 21:56:15 -0500

    previo a la actualizacion de rutas

--- src/pages/Page404.tsx
+++ src/pages/Page404.tsx
@@ -1,13 +1,21 @@
 import { BiReply } from "react-icons/bi"
-import { Link } from "react-router-dom"
+import { Link, useNavigate } from "react-router-dom"
 
 export const Page404 = () => {
+
+    const navigate = useNavigate();
+
     return (
         <div className="page-404 middle">
             <div className="middle">
                 <div className="center">
+                    <Link to="/dashboard"><h3>ADDIDSPORT</h3></Link>
                     <h1>404</h1>
-                    <Link to="/"><BiReply /> Regresar</Link>
+                    <p>Algo salio mal!</p>
+                    <button onClick={() => navigate(-1)} className="btn-show red-text center">
+                        <BiReply /> Regresar
+                    </button>
+                    {/* <Link to="/dashboard" className="btn-show red-text center"><BiReply /> Regresar</Link> */}
                 </div>
             </div>
         </div>
