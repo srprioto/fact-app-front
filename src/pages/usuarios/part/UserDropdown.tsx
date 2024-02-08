2d0bb8ab231d4735bde1fe674ff6c9288bb1782a
prioto <hailprioto@gmail.com>
2024-02-08 12:26:28 -0500

    activacion de edit de usuario

--- src/pages/usuarios/part/UserDropdown.tsx
+++ src/pages/usuarios/part/UserDropdown.tsx
@@ -1,16 +1,17 @@
-import { BiListOl, BiShowAlt, BiTrash } from "react-icons/bi";
+import { BiListOl, BiPencil, BiShowAlt } from "react-icons/bi";
 
 import { DropDown } from "../../../components/DropDown"
+import { Link } from "react-router-dom";
 
 export const UserDropdown = ({ id, nombre, handlerDeleted, handlerVer, handerVerVentas }:any) => {
     return (
         <DropDown>
-            <span onClick={ () => handlerDeleted(id, nombre) }>
+            {/* <span onClick={ () => handlerDeleted(id, nombre) }>
                 <BiTrash />Eliminar
-            </span>
-            {/* <Link to={`/usuarios/${id}/edit`} >
+            </span> */}
+            <Link to={`/usuarios/${id}/edit`} >
                 <BiPencil />Editar usuario
-            </Link> */}
+            </Link>
             <span onClick={ () => handerVerVentas(id, nombre) }>
                 <BiListOl />Reg. Ventas
             </span>
